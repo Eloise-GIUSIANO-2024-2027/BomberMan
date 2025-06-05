a1efb6bc561a5938db4a6755dd3dea5450a1c82f
MARCUS-William-2326073aa <william83w2@gmail.com>
2025-06-05 11:56:20 +0200

    texture des block

--- src/main/java/org/bomberman/GameGrid.java
+++ src/main/java/org/bomberman/GameGrid.java
@@ -2,9 +2,14 @@
 package org.bomberman;
 
 import javafx.geometry.Pos;
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
+
+import java.util.Objects;
 
 // GameGrid hérite maintenant de GridPane directement
 public class GameGrid extends GridPane {
@@ -21,6 +26,7 @@ public class GameGrid extends GridPane {
         this.setAlignment(Pos.CENTER); // Centrer les éléments à l'intérieur du GridPane
         this.setHgap(1); // Très petit espace horizontal entre les cellules
         this.setVgap(1); // Très petit espace vertical entre les cellules
+        WritableImage target = new WritableImage(32, 32);
 
         // Créer les cases (rectangles) pour chaque cellule de la grille
         for (int i = 0; i < grid.length; i++) { // Itère sur les lignes (y)
@@ -29,10 +35,10 @@ public class GameGrid extends GridPane {
 
                 // Appliquer les couleurs en fonction du type de case
                 if (grid[i][j] == 1) {
-                    rectangle.setFill(Color.web("#3a3a3a")); // Gris foncé pour les murs
+                    rectangle.setFill(new ImagePattern(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/terrain/rock1.png")),32,32,false,false))); // Gris foncé pour les murs
                     // Ou utiliser une classe CSS si tu veux : rectangle.getStyleClass().add("grid-cell-wall");
                 } else {
-                    rectangle.setFill(Color.web("#5c5c5c")); // Gris légèrement plus clair pour les espaces vides
+                    rectangle.setFill(new ImagePattern(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/terrain/grass1.png")),32,32,false,false))); // Gris foncé pour les murs
                     // Ou utiliser une classe CSS si tu veux : rectangle.getStyleClass().add("grid-cell-empty");
                 }
 
