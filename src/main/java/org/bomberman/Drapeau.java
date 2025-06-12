84c3d1d83a61eff445c1a1a418fde6b15232b348
Lou <lou.contrucci@etu.univ-amu.fr>
2025-06-12 20:55:08 +0200

    javaDoc

--- src/main/java/org/bomberman/Drapeau.java
+++ src/main/java/org/bomberman/Drapeau.java
@@ -1,9 +1,16 @@
+/**
+ * Package principal de l'application Bomberman.
+ */
 package org.bomberman;
 
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+/**
+ * Représente un drapeau dans le jeu Bomberman, utilisé dans le mode "Capture The Flag".
+ * Chaque drapeau est associé à un joueur propriétaire et peut être capturé.
+ */
 public class Drapeau extends Group {
     private int gridX;
     private int gridY;
@@ -11,6 +18,15 @@ public class Drapeau extends Group {
     private Joueur_Personnage proprietaire;
     private boolean isCaptured = false;
 
+    /**
+     * Constructeur de la classe Drapeau.
+     * Crée une nouvelle instance de drapeau à une position donnée, avec un propriétaire et une couleur.
+     *
+     * @param x La position X (colonne) sur la grille.
+     * @param y La position Y (ligne) sur la grille.
+     * @param joueur Le joueur propriétaire de ce drapeau.
+     * @param couleurDrapeau La couleur visuelle du drapeau.
+     */
     public Drapeau(int x, int y, Joueur_Personnage joueur, Color couleurDrapeau) { //
         this.gridX = x ;
         this.gridY = y;
@@ -24,17 +40,43 @@ public class Drapeau extends Group {
         setTranslateY(gridY * CELL_SIZE+2);
     }
 
+    /**
+     * Retourne la position X (colonne) du drapeau sur la grille.
+     *
+     * @return La position X du drapeau.
+     */
     public int getGridX() { return gridX; }
+
+    /**
+     * Retourne la position Y (ligne) du drapeau sur la grille.
+     *
+     * @return La position Y du drapeau.
+     */
     public int getGridY() { return gridY; }
 
+    /**
+     * Retourne le joueur propriétaire de ce drapeau.
+     *
+     * @return L'objet Joueur_Personnage qui est le propriétaire.
+     */
     public Joueur_Personnage getProprietaire() {
         return proprietaire;
     }
 
+    /**
+     * Vérifie si le drapeau a été capturé.
+     *
+     * @return true si le drapeau est capturé, false sinon.
+     */
     public boolean isCaptured() {
         return isCaptured;
     }
 
+    /**
+     * Définit l'état de capture du drapeau.
+     *
+     * @param captured true pour marquer le drapeau comme capturé, false sinon.
+     */
     public void setCaptured(boolean captured) {
         isCaptured = captured;
     }
--- src/main/java/org/bomberman/Joueur.java
+++ src/main/java/org/bomberman/Joueur.java
@@ -1,32 +1,69 @@
+/**
+ * Package principal de l'application Bomberman.
+ */
 package org.bomberman;
 
 import java.io.IOException;
 
+/**
+ * Représente un joueur contrôlable dans le jeu Bomberman, étendant les fonctionnalités
+ * de base définies dans {@link Joueur_Personnage}.
+ */
 public class Joueur extends Joueur_Personnage {
 
-
+    /**
+     * Constructeur de la classe Joueur.
+     * Initialise un nouveau joueur avec sa position de départ et son numéro.
+     * Appelle le constructeur de la classe parente {@link Joueur_Personnage}.
+     *
+     * @param game L'instance du modèle de jeu.
+     * @param startX La position X (colonne) de départ du joueur sur la grille.
+     * @param startY La position Y (ligne) de départ du joueur sur la grille.
+     * @param playerNumber Le numéro identifiant ce joueur.
+     * @throws IOException Si une erreur d'entrée/sortie survient lors de la lecture du fichier de thème dans le constructeur parent.
+     */
     public Joueur(Game game, int startX, int startY, int playerNumber) throws IOException {
         super(game,startX,startY,playerNumber);
         setLayoutX(5);  // Position X de départ
         setLayoutY(5);
     }
 
+    /**
+     * Surcharge de la méthode {@code deplacerAGauche()} de la classe parente.
+     * Déplace le joueur d'une case vers la gauche.
+     */
     @Override
     public void deplacerAGauche() {
         super.deplacerAGauche();
 
     }
 
+    /**
+     * Surcharge de la méthode {@code deplacerADroite()} de la classe parente.
+     * Déplace le joueur d'une case vers la droite.
+     *
+     * @param largeurJeu La largeur totale du jeu (passée au parent).
+     */
     @Override
     public void deplacerADroite(double largeurJeu) {
         super.deplacerADroite(largeurJeu);
     }
 
+    /**
+     * Surcharge de la méthode {@code deplacerEnBas()} de la classe parente.
+     * Déplace le joueur d'une case vers le bas.
+     *
+     * @param hauteurJeu La hauteur totale du jeu (passée au parent).
+     */
     @Override
     public void deplacerEnBas(double hauteurJeu) {
         super.deplacerEnBas(hauteurJeu);
     }
 
+    /**
+     * Surcharge de la méthode {@code deplacerEnHaut()} de la classe parente.
+     * Déplace le joueur d'une case vers le haut.
+     */
     @Override
     public void deplacerEnHaut() {
         super.deplacerEnHaut();
